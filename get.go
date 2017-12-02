bfe644bc8aa71e0f51bcd2ae8d1769663483f09f
Robin Mitra <robinmitra1@gmail.com>
2017-12-02 21:53:00 +0000

    Add the ability to filter by category, by passing a query param, and move `Quote` to separate file.

--- get.go
+++ get.go
@@ -1,43 +1,33 @@
-package hello
+package quote
 
 import (
 	"encoding/json"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
-
-type Quote struct {
-	Author string `json:"author"`
-	Quote  string `json:"quote"`
-}
-
-var quotes = []*Quote{
-	{
-		Author: "Paula Scher",
-		Quote:  "Less is more and more is more. It’s the middle that’s not a good place.",
-	},
-	{
-		Author: "Howard Aiken",
-		Quote:  "Don’t worry about people stealing an idea. If it’s original, you will have to ram it down their throats.",
-	},
-	{
-		Author: "Jack Kerouac",
-		Quote:  "Great things are not accomplished by those who yield to trends and fads and popular opinion.",
-	},
-	{
-		Author: "Frank Chimero",
-		Quote:  "To make pearls, you've got to eat dirt.",
-	},
-	{
-		Author: "Cassie McDaniel",
-		Quote:  "Designers must be stewards of design rather than dictators.",
-	},
-}
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	category := r.URL.Query().Get("category")
+	log.Debugf(ctx, "Category: %v", category)
+	q := datastore.NewQuery("Quote")
+	if len(category) != 0 {
+		q = q.Filter("Category =", category)
+	}
+	// var quotes []Quote
+	var quotes = make([]Quote, 0)
+	_, err := q.GetAll(ctx, &quotes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	response, err := json.Marshal(quotes)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
