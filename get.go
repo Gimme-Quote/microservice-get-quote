0f10fb8eb690db0c5b410d5a04e8f705cf9370c9
Robin Mitra <robinmitra1@gmail.com>
2017-12-13 16:29:38 +0000

    Add the ability to specify limit to query.

--- get.go
+++ get.go
@@ -3,6 +3,7 @@ package quote
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
@@ -15,11 +16,16 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-	category := r.URL.Query().Get("category")
-	log.Debugf(ctx, "Category: %v", category)
+	query := r.URL.Query()
+	category := query.Get("category")
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	log.Debugf(ctx, "Category: %v, Limit: %v", category, limit)
 	q := datastore.NewQuery("Quote")
 	if len(category) != 0 {
 		q = q.Filter("Category =", category)
+	}
+	if limit > 0 {
+		q = q.Limit(limit)
 	}
 	// var quotes []Quote
 	var quotes = make([]Quote, 0)
